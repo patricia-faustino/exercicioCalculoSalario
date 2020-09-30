08afdb5436c3b53d673df0294471594deb3e14c7
Patricia <patriciafaustinobr@gmail.com>
2020-09-30 19:51:16 -0300

    Fiz exercicio para calculo de salario a partir das horas trabalhadas

---
+++ exerc3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace exerc3 {
+    class Program {
+        static void Main(string[] args) {
+            int num;
+            double horas, salario, valor;
+            Console.WriteLine("Digite seu número: ");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite as horas trabalhadas: ");
+            horas = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor recebido por hora: ");
+            valor = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            salario = horas * valor;
+            Console.WriteLine("NUMBER= " +num);
+            Console.WriteLine("SALARY =U$" + salario.ToString("F2",CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
